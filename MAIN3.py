51a15787cc15c205e036b5b780c298574ddd6136
FranciscoJoseKroff <franciscojose.kroff@alumnos.ulagos.cl>
2025-04-27 22:32:40 -0300

    Add files via upload

---
+++ MAIN3.py
@@ -0,0 +1,26 @@
+import tkinter as tk 
+from tkinter import ttk
+from scripts.gestion_usuarios3 import ventanaAcceso
+
+
+def Main():
+        
+    root = tk.Tk()
+    root.title("Proyecto TPA")
+    root.geometry("1280x720")
+    root.resizable(False,False)
+    root.configure(background="gray")
+    
+    frame = ttk.Frame(root,width=1270,height=700,) 
+    frame.pack(pady=10, padx=10)
+    frame.pack_propagate(False)
+    
+    ventanaAcceso(frame)   
+    
+    root.mainloop()
+ 
+
+if __name__ == "__main__":
+    Main()
+
+    
