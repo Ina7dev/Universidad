3afa688ae0806b42ade597ff54a2b6418703cd3e
xCr1s12 <CristobalAlexis.onate@alumnos.ulagos.cl>
2025-07-10 17:47:05 -0400

    cambios en imports

--- scripts/gestion_usuarios2.py
+++ scripts/gestion_usuarios2.py
@@ -1,10 +1,10 @@
-from users import User
-from gestion_clientes import ventanaCRUDclientes, ventanaCliente
-from gestion_rol import ventanaCRUDroles, cargarRoles
-from gestion_reservas import ventanaCRUDreservas
-from gestion_habitaciones import GestionHabitaciones
-from gestion_servicios import ventanaCRUDservicios 
-from gestion_mantenimiento import ventanaCRUDMantenimiento
+from .users import User
+from .gestion_clientes import ventanaCRUDclientes, ventanaCliente
+from .gestion_rol import ventanaCRUDroles, cargarRoles
+from .gestion_reservas import ventanaCRUDreservas
+from .gestion_habitaciones import GestionHabitaciones
+from .gestion_servicios import ventanaCRUDservicios 
+from .gestion_mantenimiento import ventanaCRUDMantenimiento
 
 import tkinter as tk
 from tkinter import ttk
